63d57fe2933b10bb3c53c8c5f9c3fa3aa9c50374
jake-bc <jake.smith@bigcommerce.com>
2019-03-13 22:51:57 -0500

    fix it

--- routes/auth.js
+++ routes/auth.js
@@ -2,13 +2,10 @@ var express = require('express');
 var router = express.Router();
 var B = require('../config/keys').B
 
-router.get('/', (req, res, next) => {
-    B.authorize(req.query, (data) => {
-      console.log(data)
-      return data;
-    })
-      .then(data => res.render('home', { title: 'Authorized!', data: data }))
-      .catch(next);
-    });
+router.get('/auth', (req, res, next) => {
+  B.authorize(req.query)
+    .then(data => res.render('integrations/auth', { title: 'Authorized!', data: data }))
+    .catch(next);
+  });
 
  module.exports = router;
