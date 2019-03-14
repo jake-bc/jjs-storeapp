e9ba0e3993f8cab2c379e7ad5cc962e9c2702756
jake-bc <jake.smith@bigcommerce.com>
2019-03-13 21:27:54 -0500

    make it better

--- server.js
+++ server.js
@@ -1,17 +1,13 @@
-var path = require('path'),
-    express = require('express'),
-    request = require('request'),
+var express = require('express'),
     exphbs = require('express-handlebars'),
     mongoose = require("mongoose"),
-    assert = require('assert'),
-    app = express(),
     cors = require('cors'),
     bodyParser = require("body-parser"),
     auth = require('./routes/auth'),
     load = require('./routes/load'),
     products = require('./routes/catalog/products')
-    router = express.Router();
 
+const app = express();
 
 app.engine('handlebars', exphbs({
     defaultLayout: 'main'
@@ -21,32 +17,34 @@ app.set('view engine', 'handlebars');
 // Connect to MongoDB
 const url = process.env.MLABS;
 mongoose
-.connect(url, function(err, db) {
+    .connect(url, function (err, db) {
 
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("connected");
-      
-      db.collection('Stores', function(err, collection) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("connected");
 
-        collection.insert({
-            id: 1,
-            storeHash: process.env.STORE_HASH,
-            accessToken: process.env.ACCESS_TOKEN,
-            scope: process.env.SCOPE
-        });
+            db.collection('Stores', function (err, collection) {
 
-        db.collection('Stores').count(function(err, count) {
+                collection.insert({
+                    id: 1,
+                    storeHash: process.env.STORE_HASH,
+                    accessToken: process.env.ACCESS_TOKEN,
+                    scope: process.env.SCOPE
+                });
 
-            console.log('Total Rows: ' + count);
-        });
+                db.collection('Stores').count(function (err, count) {
+
+                    console.log('Total Rows: ' + count);
+                });
+            });
+        }
     });
-    }
-});
 
 app.use(cors())
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(bodyParser.json());
 
 // http://expressjs.com/en/starter/static-files.html
@@ -58,13 +56,11 @@ app.use('/load', load);
 app.use('/products', products);
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
     res.render('home');
 });
-  
+
 // listen for requests :)
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
-
-module.exports = app;
